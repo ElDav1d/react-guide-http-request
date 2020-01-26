3487772465d2deea25b6a5a91542fda179877783
David Vivó <david.vivo@oramon.nl>
2020-01-26 14:17:33 +0100

    Handle with post GET errors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,11 +9,13 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
+
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then(response => {
+            .then( response => {
                 const posts = response.data.slice(0,4);
                 const updatedPosts = posts.map(post => {
                     return {
@@ -23,6 +25,9 @@ class Blog extends Component {
                 })
                 this.setState({posts: updatedPosts});
                 // console.log(response);
+            } )
+            .catch(error => {
+                this.setState({error: true});
             });
     }
 
@@ -31,14 +36,17 @@ class Blog extends Component {
     }
 
     render () {
-        const posts = this.state.posts.map(post => {
+        let posts = <p style={{textAlign: "center"}}><strong>Something went wrong!</strong></p>;
+        if (!this.state.error) {
+            posts = this.state.posts.map(post => {
             return <Post
                 key={post.id}
                 title={post.title}
                 author={post.author}
                 clicked={() => this.postSelectedHandler(post.id)} />
-        });
-        
+            });
+        }
+
         return (
             <div>
                 <section className="Posts">
